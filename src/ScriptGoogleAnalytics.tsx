99ba4c99040aac3a739e038e31c64911b163b6c8
luigiminardim <luigiminardim@gmail.com>
2022-07-28 19:24:58 -0300

    feat: Use Google Tag Manager

--- src/ScriptGoogleAnalytics.tsx
+++ src/ScriptGoogleAnalytics.tsx
@@ -2,11 +2,13 @@ import Head from "next/head";
 import Script from "next/script";
 import { useEffect, useState } from "react";
 
-const analyticsScript = `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-P1P5ZVZ361');`;
+const analyticsScript = `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THBHZKK');
+`;
 
 export function ScriptGoogleAnalytics() {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,12 +22,7 @@ export function ScriptGoogleAnalytics() {
   return (
     <>
       <Script
-        strategy="afterInteractive"
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-P1P5ZVZ361"
-      ></Script>
-      <Script
-        id="Script-GoogleAnalytics"
+        id="Script-GoogleTagManager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{ __html: analyticsScript }}
       />
